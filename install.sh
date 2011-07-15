049ad90f1bce00f1bfe60ccf5e2b7450e10260d0
Lubosz Sarnecki <lubosz@gmail.com>
2011-07-15 03:57:14 +0200

    check for existing clones and builds of dependencies. check for installed opencv 2.3.

--- install.sh
+++ install.sh
@@ -6,97 +6,209 @@
 CPU_CORES=`getconf _NPROCESSORS_ONLN`
 GCC_THREADS=`expr $CPU_CORES + 1`
 
+function error {
+  echo "ERROR $1 could not be build"
+  exit
+}
+
 if uname -a | grep Ubuntu; then
-  sudo apt-get install build-essential git subversion cvs qt4-qmake cmake libunicap-dev libucil-dev libcv-dev liblapack-dev gfortran libf2c2-dev libftgl-dev libcvaux-dev libhighgui-dev
+  sudo apt-get install build-essential git subversion cvs qt4-qmake cmake libunicap-dev libucil-dev liblapack-dev gfortran libf2c2-dev libftgl-dev libhighgui-dev
 fi
 
-mkdir external
+if [ ! -d external ]; then
+  mkdir external
+fi
+  
 cd external/
 
-svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/coreDataStructs/lib coreDataStructs
+#
+# coreDataStructs
+#
+if [ ! -d coreDataStructs ]; then
+  svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/coreDataStructs/lib coreDataStructs
+else
+  svn up coreDataStructs
+fi
 
+#
+# imageGrabber
+#
 #svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/imageGrabber/lib imageGrabber
 #cd imageGrabber
 #qmake
 #make -j$GCC_THREADS
 #cd ..
 
-svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/imageOperations/lib imageOperations
-cd imageOperations
-patch -p0 < ../../patches/imageOperationsPaths.diff
-qmake
-make -j$GCC_THREADS
-cd ..
+#
+# imageOperations
+#
+if [ ! -d imageOperations ]; then
+  svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/imageOperations/lib imageOperations
+else
+  svn up imageOperations
+fi
 
-svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/tinyxml
-cd tinyxml
-qmake
-make -j$GCC_THREADS
-cd ..
+if [ ! -f imageOperations/libimageOperations.a ]; then
+  cd imageOperations
+  patch -p0 < ../../patches/imageOperationsPaths.diff
+  qmake
+  make -j$GCC_THREADS || error "imageOperations"
+  cd ..
+fi
 
-git clone git://github.com/mariusmuja/flann.git
-cd flann
-git checkout 94d28f2d2868709adac1c944472dda71f5b8c289
-cmake . -DBUILD_MATLAB_BINDINGS=0 -DBUILD_PYTHON_BINDINGS=0 -DLATEX_OUTPUT_PATH=.
-make -j$GCC_THREADS
-cd ..
+#
+# tinyxml
+#
+if [ ! -d tinyxml ]; then
+  svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/external/tinyxml
+else
+  svn up tinyxml
+fi
 
-svn co https://lapackpp.svn.sourceforge.net/svnroot/lapackpp/lapackpp/trunk/ lapackpp
-cd lapackpp
-./autogen.sh
-./configure
-make -j$GCC_THREADS
-ln -s include lapackpp
-cd ..
+if [ ! -f tinyxml/libtinyxml.a ]; then
+  cd tinyxml
+  qmake
+  make -j$GCC_THREADS || error "tinyxml"
+  cd ..
+fi
 
-cvs -z3 -d:pserver:anoncvs@cvs.savannah.nongnu.org:/cvsroot/toon co TooN
+#
+# flann
+#
+if [ ! -d flann ]; then
+  git clone git://github.com/mariusmuja/flann.git
+fi
+
+if [ ! -f flann/lib/libflann.so ]; then
+  cd flann
+  git checkout 94d28f2d2868709adac1c944472dda71f5b8c289
+  cmake . -DBUILD_MATLAB_BINDINGS=0 -DBUILD_PYTHON_BINDINGS=0 -DLATEX_OUTPUT_PATH=.
+  make -j$GCC_THREADS || error "flann"
+  cd ..
+fi
+
+#
+# lapackpp
+#
+if [ ! -d lapackpp ]; then
+  svn co https://lapackpp.svn.sourceforge.net/svnroot/lapackpp/lapackpp/trunk/ lapackpp
+fi
+
+if [ ! -f lapackpp/src/.libs/liblapackpp.so ]; then
+  cd lapackpp
+  ./autogen.sh
+  ./configure
+  make -j$GCC_THREADS || error "lapackpp"
+  ln -s include lapackpp
+  cd ..
+fi
+
+#
+# TooN
+#
+if [ ! -d TooN ]; then
+  cvs -z3 -d:pserver:anoncvs@cvs.savannah.nongnu.org:/cvsroot/toon co TooN
+fi
 #cd TooN
 #./configure
 #cd ..
 
-cvs -z3 -d:pserver:anoncvs@cvs.savannah.nongnu.org:/cvsroot/toon co tag
-cd tag
-chmod +x configure
-./configure --with-TooN=..
-#patch wrong header case
-sed -i 's/svd.h/SVD.h/g' tag/absorient.h 
-make -j$GCC_THREADS
+#
+# tag
+#
+if [ ! -d tag ]; then
+  cvs -z3 -d:pserver:anoncvs@cvs.savannah.nongnu.org:/cvsroot/toon co tag
+fi
+
+if [ ! -f tag/.libs/libtoontag.a ]; then
+  cd tag
+  chmod +x configure
+  ./configure --with-TooN=..
+  #patch wrong header case
+  sed -i 's/svd.h/SVD.h/g' tag/absorient.h 
+  make -j$GCC_THREADS || error "tag"
+  cd ..
+fi
+
+#
+# artoolkit
+#
+#svn co https://artoolkit.svn.sourceforge.net/svnroot/artoolkit/trunk/artoolkit/
+#cd artoolkit
+#./Configure
+#make -j$GCC_THREADS
+#cd ..
+
+#
+# opencv
+#
+
+#Look for installed opencv 2.3
+if [ ! -f /usr/lib/libopencv_imgproc.so.2.3 ]; then
+  if [ ! -d OpenCV-2.3.0 ]; then
+    wget http://freefr.dl.sourceforge.net/project/opencvlibrary/opencv-unix/2.3/OpenCV-2.3.0.tar.bz2
+    tar -xvjf OpenCV-2.3.0.tar.bz2
+    rm OpenCV-2.3.0.tar.bz2
+  fi
+
+  if [ ! -f OpenCV-2.3.0/lib/libopencv_imgproc.so ]; then
+    cd OpenCV-2.3.0
+    cmake .
+    make -j$GCC_THREADS || error "OpenCV"
+    cd ..
+  fi
+fi
+
+#
+# sba
+#
+if [ ! -d sba ]; then
+  wget http://www.ics.forth.gr/~lourakis/sba/sba-1.6.tgz
+  tar xvfz sba-1.6.tgz
+  rm sba-1.6.tgz
+  mv sba-1.6 sba
+fi
+
+if [ ! -f sba/libsba.a ]; then
+  cd sba
+  cmake . -Wno-dev
+  make -j$GCC_THREADS || error "sba"
+  cd ..
+fi
+
 cd ..
 
-svn co https://artoolkit.svn.sourceforge.net/svnroot/artoolkit/trunk/artoolkit/
-cd artoolkit
-./Configure
-make -j$GCC_THREADS
-cd ..
+#
+# baselib
+#
+if [ ! -d baselib ]; then
+  svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/baselib
+else
+  svn up baselib
+fi
 
-wget http://freefr.dl.sourceforge.net/project/opencvlibrary/opencv-unix/2.3/OpenCV-2.3.0.tar.bz2
-tar -xvjf OpenCV-2.3.0.tar.bz2
-rm OpenCV-2.3.0.tar.bz2
-cd OpenCV-2.3.0
-cmake .
-make -j$GCC_THREADS
-cd ..
+if [ ! -f baselib/libbaselib.a ]; then
+  cd baselib
+  patch -p0 < ../patches/baselibBuild.diff
+  cmake . -DRELEASE=1
+  make -j$GCC_THREADS || error "baselib"
+  cd ..
+fi
 
-wget http://www.ics.forth.gr/~lourakis/sba/sba-1.6.tgz
-tar xvfz sba-1.6.tgz
-rm sba-1.6.tgz
-mv sba-1.6 sba
-cd sba
-cmake . -Wno-dev
-make -j$GCC_THREADS
-cd ../..
+#
+# baselibApp
+#
+if [ ! -d baselibApp ]; then
+  svn co https://svn.uni-koblenz.de/decker/baselibApp
+else
+  svn up baselibApp
+fi
 
-svn co https://svn.uni-koblenz.de/agas/projects/IntegVelodyne/30_prog/baselib
-cd baselib
-patch -p0 < ../patches/baselibBuild.diff
-cmake . -DRELEASE=1
-make -j$GCC_THREADS
-cd ..
-
-svn co https://svn.uni-koblenz.de/decker/baselibApp
 cd baselibApp
+#make clean
+#rm CMakeCache.txt
 cmake . -DRELEASE=1
-make -j$GCC_THREADS
+make -j$GCC_THREADS || error "baselibApp"
 echo "Done"
 
 
